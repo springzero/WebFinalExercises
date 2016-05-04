9d9a3f925fb79928e16dd5f1f5c86161061a400d
springzero <2680064189@qq.com>
2016-05-04 23:17:13 +0800

    chapter3一个web项目空壳， smart-framework刚弄完LoadClass类的定义

---
+++ smart-framework/src/main/java/org/smart4j/framework/util/StringUtil.java
@@ -0,0 +1,30 @@
+package org.smart4j.framework.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 字符串工具类
+ * Created by Administrator on 2016/4/27.
+ */
+public final class StringUtil {
+
+    /*
+    * 判断字符串是否为空
+    * */
+    public static boolean isEmpty(String str) {
+        if(str != null) {
+            str = str.trim();
+        }
+        return StringUtils.isEmpty(str);
+    }
+
+
+    /*
+    * 判断字符串是否非空
+    * */
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+
+
+}
